936e6f0a5b9439097ea0cec81c507ccae6a0e54e
Matthew Jeong <mangjee.24@gmail.com>
2025-09-16 13:02:36 -0400

    testing

--- pset1/main.c
+++ pset1/main.c
@@ -1,9 +1,9 @@
 #include "pset1lib.h" 
 
-int main() {
+int main(int argc, char **argv) {
     
-    struct STREAM *file1 = myfopen("testinput.txt", "r");
-    struct STREAM *file2 = myfopen("testoutput.txt", "w");
+    struct STREAM *file1 = myfopen(argv[1], "r");
+    struct STREAM *file2 = myfopen(argv[2], "w");
     
     int a;
     while((a = myfgetc(file1)) != EOF) {

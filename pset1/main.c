4c9f38dd68e56f5c684e425e09b9d83deb09a4e8
Matthew Jeong <mangjee.24@gmail.com>
2025-09-16 13:23:38 -0400

    final working version

--- pset1/main.c
+++ pset1/main.c
@@ -1,5 +1,5 @@
 #include "pset1lib.h" 
-
+#include <stdio.h>
 int main(int argc, char **argv) {
     
     struct STREAM *file1 = myfopen(argv[1], "r");
@@ -7,8 +7,10 @@ int main(int argc, char **argv) {
     
     int a;
     while((a = myfgetc(file1)) != EOF) {
-        if(a == 9) {
-            myfputc(32, file2);
+        if(a == 0x09) {
+            for(int i = 0; i < 4; i++) {
+                myfputc(0x20, file2);
+            }
         }
 
         else {
@@ -16,8 +18,12 @@ int main(int argc, char **argv) {
         }
     }
     
-    myfclose(file2);
-    myfclose(file1);
+    if(myfclose(file2) == 0 && myfclose(file1) == 0) {
+        return 0;
+    }
 
-    
+    else {
+        return 255;
+    }
+
 }

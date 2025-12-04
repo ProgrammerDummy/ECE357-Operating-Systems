49a61df09eb7f53d17f0398df126eb03ee312c21
Myung Jae (Matthew) Jeong <63801315+ProgrammerDummy@users.noreply.github.com>
2025-12-03 23:01:10 -0500

    sem.h updated accordingly

--- pset6/sem.h
+++ pset6/sem.h
@@ -1,6 +1,8 @@
 #include "spinlock.h"
 
+#define my_procnum 64
+
 void sem_init(struct sem *s, int count);
-void sem_try(struct sem *s);
+int sem_try(struct sem *s);
 void sem_wait(struct sem *s);
 void sem_inc(struct sem *s);

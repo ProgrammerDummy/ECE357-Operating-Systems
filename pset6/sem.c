8248f5e90a154ee1fa17a7ab9c97c6ccba7c1a2b
Matthew Jeong <mangjee.24@gmail.com>
2025-12-04 22:02:16 -0500

    deadlock fixed

--- pset6/sem.c
+++ pset6/sem.c
@@ -1,13 +1,8 @@
 #include "sem.h"
 
-struct sem {
-    volatile unsigned int semaphore; //protects the data_example
-    volatile char semaphore_lock; //protects the semaphore integer 
-    pid_t wait_list[n_proc]; //keeps track of processes waiting 
-    int sleep_list[n_proc]; //flag array (0 = awake, 1 = asleep)
-};
+int my_procnum = 0;
 
-void handler() {
+void blank_handler(int sn) {
     //does nothing, just prevents process from terminating
 }
 
@@ -15,33 +10,33 @@ void sem_init(struct sem *s, int count) {
     s->semaphore = count;
     s->semaphore_lock = 0; //start off as unlocked obviously
 
+    // // early return if count is larger than our max process number
+    // if(!(count <= N_PROC)) {
+    //     fprintf(stdout, "count is larger than maximum resource pool");
+    //     return;
+    // }
 
-    if(!(count <= my_procnum)) {
-        fprintf(stdout, "count is larger than maximum resource pool");
-        return -1;
-    }
-
-    for(int i = 0; i < n_proc; i++) {
+    for(int i = 0; i < N_PROC; i++) {
         s->wait_list[i] = 0;
         s->sleep_list[i] = 0;
     }
 
-    s->wait_list[my_procnum] = getpid();
+    // s->wait_list[my_procnum] = getpid();
 
 
     struct sigaction SIGUSR1_response;
-
     SIGUSR1_response.sa_flags = 0;
-    SIGUSR1_response.sa_mask = 0;
-    SIGUSR1_response.sa_sigaction = handler;
+    sigemptyset(&SIGUSR1_response.sa_mask);
+    SIGUSR1_response.sa_handler = blank_handler;
 
     if(sigaction(SIGUSR1, &SIGUSR1_response, NULL) == -1) {
-        perror("sigaction for assigning new handler for SIGUSR1 failed");
+        fprintf(stderr, "Sigaction for assigning new handler for SIGUSR1 failed: %s", strerror(errno));
         return;
     }
 
     //index is the next element (process) TO BE added, meaning currently no waiting process id is stored in wait_list[wait_list_index]
 }
+
 
 int sem_try(struct sem *s) {  //attempt P operation
 
@@ -54,6 +49,7 @@ int sem_try(struct sem *s) {  //attempt P operation
     sigfillset(&newset); //blocks all signals
 
     if(sigprocmask(SIG_SETMASK, &newset, &oldset) == -1) {
+        fprintf(stderr, "Initial signal masking in sem_try failed: %s", strerror(errno));
         perror("signal masking failed");
     }
 
@@ -69,13 +65,12 @@ int sem_try(struct sem *s) {  //attempt P operation
 
     spin_unlock(&s->semaphore_lock);
 
-    return 1;
-    
     if(sigprocmask(SIG_SETMASK, &oldset, NULL) == -1) { //reset original mask
-        perror("signal masking failed");
-        return -1;
+        fprintf(stderr, "Signal mask restoration failed in sem_try after semaphore was decremented: %s", strerror(errno));
+        // no return here because the sem_try still worked
     }
-    
+
+    return 1;
 }
 
 void sem_wait(struct sem *s) { // P operation
@@ -89,28 +84,29 @@ void sem_wait(struct sem *s) { // P operation
     sigfillset(&newset); //blocks all signals
 
     if(sigprocmask(SIG_SETMASK, &newset, &oldset) == -1) {
-        perror("signal masking failed");
+        fprintf(stderr, "Initial signal masking in sem_wait failed: %s", strerror(errno));
     }
 
     spin_lock(&s->semaphore_lock);
 
     while(s->semaphore == 0) {
+        s->wait_list[my_procnum] = getpid();
         s->sleep_list[my_procnum] = 1; //add the current process to list of processes in wait list
         spin_unlock(&s->semaphore_lock);
-        
-        sigprocmask(SIG_SETMASK, &oldset, NULL); //reset signal mask to original
+        // sigprocmask(SIG_SETMASK, &oldset, NULL); //reset signal mask to original
         sigemptyset(&newset);
         sigsuspend(&newset); //any signal works, goes to sleep (blocks)
         spin_lock(&s->semaphore_lock);
     }
     
     s->semaphore--;
+    s->sleep_list[my_procnum] = 0; // clear sleep flag now
 
     spin_unlock(&s->semaphore_lock);
 
     
     if(sigprocmask(SIG_SETMASK, &oldset, NULL) == -1) { //reset original mask
-        perror("signal masking failed");
+        fprintf(stderr, "Signal mask restoration failed in sem_wait after semaphore was decremented: %s", strerror(errno));
     }
 
 }
@@ -126,27 +122,27 @@ void sem_inc(struct sem *s) { // V operation
     sigfillset(&newset); //blocks all signals
 
     if(sigprocmask(SIG_SETMASK, &newset, &oldset) == -1) { //mask all signals
-        perror("signal masking failed");
+        fprintf(stderr, "Initial signal masking in sem_inc failed: %s", strerror(errno));
     }
     
     spin_lock(&s->semaphore_lock);
 
     s->semaphore++;
 
-    for(int i = 0; i < n_proc; i++) {
-        kill(s->wait_list[i], SIGUSR1); //wakes up every sleeping process
-    } 
-
-    for(int i = 0; i < n_proc; i++) {
-        s->wait_list[i] = 0; //empties the waitlist
+    for(int i = 0; i < N_PROC; i++) {
+        // only wake sleeping processes and handle edge case incase parent pid does something weird
+        if ((s->sleep_list[i] == 1) && (s->wait_list[i] != 0)) {
+            kill(s->wait_list[i], SIGUSR1); // wakes up every sleeping process
+            s->sleep_list[i] = 0; // empties sleep list
+            s->wait_list[i] = 0; // empties wait list
+        }
     }
 
     spin_unlock(&s->semaphore_lock);
 
     if(sigprocmask(SIG_SETMASK, &oldset, NULL) == -1) { //reset original mask
-        perror("signal masking failed");
+        fprintf(stderr, "Signal mask restoration failed in sem_inc after semaphore was incremented: %s", strerror(errno));
     }
-
-
-
 }
+
+
--- pset6/sem.h
+++ pset6/sem.h
@@ -1,5 +1,14 @@
+#ifndef SEM_H
+
 #include "spinlock.h"
-#define n_proc 64
+#define N_PROC 64
+
+struct sem {
+    volatile unsigned int semaphore; //protects the data_example
+    volatile char semaphore_lock; //protects the semaphore integer 
+    pid_t wait_list[N_PROC]; //keeps track of processes waiting 
+    int sleep_list[N_PROC]; //flag array (0 = awake, 1 = asleep)
+};
 
 extern int my_procnum;
 
@@ -7,3 +16,6 @@ void sem_init(struct sem *s, int count);
 int sem_try(struct sem *s);
 void sem_wait(struct sem *s);
 void sem_inc(struct sem *s);
+
+#define SEM_H
+#endif
